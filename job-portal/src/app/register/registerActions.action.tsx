d707153ba2d5a69ba2ce09d7aa60f160c77f30d0
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-05-02 14:14:44 +0530

    validtion on email or username

--- job-portal/src/app/register/registerActions.action.tsx
+++ job-portal/src/app/register/registerActions.action.tsx
@@ -3,6 +3,7 @@
 import { db } from "@/config/db";
 import { users } from "@/drizzle/schema";
 import  argon2  from "argon2";
+import { eq, or } from "drizzle-orm";
 
 export const registerAction = async (data: {
     name: string;
@@ -14,6 +15,23 @@ export const registerAction = async (data: {
 
     try {
     const {name,userName,email,password,role}=data
+
+    const [user] = await db.select().from(users).where(or(eq(users.email,email),eq(users.userName,userName))) 
+    
+    if(user){
+       if(user.email === email){
+        return {
+            status: "error",
+            message: "Email already exists"
+        }
+        }
+        else if(user.userName === userName){
+            return {
+                status: "error",
+                message: "Username already exists"
+            }
+       }
+    }    
     const hashPassword = await argon2.hash(password)
     await db.insert(users).values({name,userName,email,password:hashPassword,role})
     return {
